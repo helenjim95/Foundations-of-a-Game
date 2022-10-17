dc92b27a7b477fc0d15bea08006ea6c9a41ebac6
Alp Kaan Aksu <alpkaan.aksu@tum.de>
2022-10-17 14:17:55 +0200

    Exercise-Template pushed by Artemis

---
+++ src/de/tum/in/ase/Game.java
@@ -0,0 +1,17 @@
+package de.tum.in.ase;
+
+public class Game {
+    //TODO: implement the constructor, getters and setters for all the attributes
+    //TODO: implement the move methods
+    //TODO: implement a boolean method "isWon()" which checks if the game has ended (the player has won)
+    //TODO: finally runGame()
+
+
+    //You can run your game on main after you've implemented runGame()
+    public static void main(String[] args) {
+        //Game game = new Game(5, 5);
+        //game.runGame();
+    }
+
+
+}
---
+++ src/de/tum/in/ase/Hero.java
@@ -0,0 +1,5 @@
+package de.tum.in.ase;
+
+public class Hero {
+    // TODO: Implement the Hero class
+}
